d503ccb82a3dfafbe6d183e81cceb053226d476e
Carmen Grajeda <carmengrajeda@westernwyoming.edu>
2025-01-21 16:05:20 +0000

    Ready for grading.

--- hello-world/main.cpp
+++ hello-world/main.cpp
@@ -1,7 +1,14 @@
 //
-// Name
-// Date
+// Carmen Grajeda
+// 1/21/2025
 // Hello World Programming Project
 // COSC 1030
 //
 
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!";
+    return 0;
+}
