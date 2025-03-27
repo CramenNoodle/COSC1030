074425390bbc833c5de562b5b6aa11aa5ba0519c
Carmen Grajeda <carmengrajeda@westernwyoming.edu>
2025-03-27 18:13:14 +0000

    Ready to Grade

--- temperatures/instructions.md
+++ temperatures/instructions.md
@@ -2,11 +2,3 @@
 
 Write a program that will input temperatures for consecutive days. The program will store these values into an array and call a function that will return the average of the temperatures. It will also call a function that will return the highest temperature and a function that will return the lowest temperature. The user will input the number of temperatures to be read. There will be no more than 50 temperatures. Use `typedef` to declare the array type. The average should be displayed to two decimal places.
 
-1. Input temperatures for consective days
-2. store in array
-    for ( count = 1; count <= Num_days; count++)
-        {
-            cout <<" Enter the number of days "
-            << count << ": ";
-            cin >> hours[count - 1];
-        }
--- temperatures/main.cpp
+++ temperatures/main.cpp
@@ -5,16 +5,110 @@
 // COSC 1030
 //
 
-#include <iostream>
+// Temperature Program
 
+#include <iostream>
+#include <iomanip>
 using namespace std;
 
+// Use "Typedef" to define an array type for temperatures
+typedef double TemperatureArray[50];
+// Function Prototypes
+void getTemperatures(TemperatureArray, int);
+double getAverage(const TemperatureArray, int);
+double getHighestTemp(const TemperatureArray, int);
+double getLowestTemp(const TemperatureArray, int);
+
 int main()
-{   
+{
     // Variables
-    const double TEMPERATURES = 100; // Constant for the array size
-    double temp [TEMPERATURES]; // Array with 100 El 
-    int count 
-    
+    TemperatureArray dailyTemp; // Array to store temperatures
+    int numTemps; // Variable for the number of temperatures 
+    double average, highestTemp, lowestTemp; // Variables for the average, highest, and lowest temperature 
+
+    // Ask the user how many temperatures they want to enter 
+    cout << "Enter the number of temperatures (max 50): ";
+    cin >> numTemps;
+
+    // Valiable input 
+    while (numTemps < 1 || numTemps > 50)
+    {
+        cout << "Invalid input. Please enter a number from 1 - 50: ";
+        cin >> numTemps;
+    }
+
+    // Get the temperature from the user
+    getTemperatures(dailyTemp, numTemps);
+
+    // Get the average, highest, and lowest of the temperature
+    average = getAverage(dailyTemp, numTemps);
+    highestTemp = getHighestTemp(dailyTemp, numTemps);
+    lowestTemp = getLowestTemp(dailyTemp, numTemps);
+
+    // Display the average, highest, and lowest temperatures with two decimal points.
+    cout << fixed << showpoint << setprecision(2);
+    cout << "\nThe average temperature is: " << average << ".\n";
+    cout << "The highest temperature was: " << highestTemp << ".\n";
+    cout << "The lowest temperature was: " << lowestTemp << ".\n";
+
+    return 0;
+
 }
 
+/*          getTemperatures
+This function asks the user to enter temperatures and stores them in an array
+*/
+void getTemperatures(TemperatureArray dailyTemp, int numTemps)
+{
+    for(int index = 0; index < numTemps; index++)
+    {
+        cout << "Enter the Temperature " << (index + 1) << ": ";
+        cin >> dailyTemp[index];
+    }
+}
+
+/*            getAverage
+This function calculates and returns the average of all the temperatures
+*/
+double getAverage(const TemperatureArray dailyTemp, int numTemps)
+{
+    double total = 0; // Accumulator
+
+    // Add each element to total.
+    for(int count = 0; count < numTemps; count++)
+    {
+        total += dailyTemp[count];
+    }
+
+    return total / numTemps;
+}
+
+/*      getHighestTemp
+This function returns the highest temperature in the array*/
+double getHighestTemp(const TemperatureArray dailyTemp, int numTemps)
+{
+    double highest = dailyTemp[0];
+    for (int count = 1; count < numTemps; count++)
+    {
+        if (dailyTemp[count] > highest)
+        {
+            highest = dailyTemp[count];
+        }
+    }
+    return highest;
+}
+
+/*         getLowestTemp
+This function returns the lowest temperature in the array*/
+double getLowestTemp(const TemperatureArray dailyTemp, int numTemps)
+{
+    double lowest = dailyTemp[0];
+    for (int count = 1; count < numTemps; count++)
+    {
+        if (dailyTemp[count] < lowest)
+        {
+            lowest = dailyTemp[count];
+        }
+    }
+    return lowest;
+}
