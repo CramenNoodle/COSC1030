1b3335828c9999d3a548d3b41dcac3202e417c59
Carmen Grajeda <carmengrajeda@westernwyoming.edu>
2025-04-24 22:31:09 +0000

    Ready to Grade

--- dental-bill/instructions.md
+++ dental-bill/instructions.md
@@ -1,3 +1,13 @@
 # Instructions  
 
 Write a program that outputs a dentist bill. For members of a dental plan, the bill consists of the service charge (for the particular procedure performed) and test fees, input to the program by the user. To non- members the charges consist of the above services plus medicine (also input by the user). The program first asks if the patient is a member of the dental plan. The program uses two overloaded functions to calculate the total bill. Both are value returning functions that return the total charge.
+
+1. ask if the patient is a member of the dental plan 
+2. members
+    - service charge for particular procedure performed
+    - test fees
+   non-members
+    - service charge 
+    - medicine charge
+    - test fees
+    
--- dental-bill/main.cpp
+++ dental-bill/main.cpp
@@ -1,7 +1,86 @@
 //
-// Name
-// Date
+// Carmen Grajeda
+// 04/24/2025
 // Dental Bill Programming Project
 // COSC 1030
 //
 
+// Dental Bill Program
+
+#include <iostream>
+
+using namespace std;
+
+// Functions
+double calculateBill(double, double); // Members
+double calculateBill(double, double, double); // Non-members
+
+int main()
+
+{
+   // Variables 
+   char member; // If they are a member or not 
+   double serviceCharge,
+          testFees,
+          medicineFees = 0.0, // Start at 0 in case they are a member
+          totalBill;
+
+    // Ask user if they are a member or not 
+    cout << "Welcome!\n";
+    cout << "Are you a member of our Dental plan? (y/n): ";
+    cin >> member;
+
+    // Get the service charge amount 
+    cout << "Enter Service Charge: $";
+    cin >> serviceCharge;
+
+    // Get the test fee charge amount 
+    cout << "Enter Test Fees: $";
+    cin >> testFees;
+
+    // Validate the user input 
+    if (member == 'y' || member == 'Y')
+    {
+        // Calculate members total bill
+        totalBill = calculateBill(serviceCharge, testFees);
+    }
+    else
+    {
+        // If not a member ask for medicine amount
+        cout << "Enter Medicine Fees: $";
+        cin >> medicineFees;
+
+        // Calculate non members total bill
+        totalBill = calculateBill(serviceCharge, testFees, medicineFees);
+    }
+
+    // Display total bill 
+    cout << "\n______ DENTIST BILL ______\n";
+    cout << "Service Charge: $" << serviceCharge << endl;
+    cout << "Test Fees: $" << testFees << endl;
+
+    // If non member display medicine fees
+    if (!(member == 'y' || member == 'Y'))
+    {
+        cout << "Medicine Fees: $" << medicineFees << endl;
+    }
+
+    cout << "______ TOTAL BILL ______: $" << totalBill << endl;
+    
+    return 0;
+    
+}
+
+// Function for members of the dentist 
+double calculateBill(double serviceCharge, double testFees)
+{
+    // add the service and test fees together
+    return serviceCharge + testFees;
+}
+
+// Function that is overloaded for non-members of the dentist
+double calculateBill(double serviceCharge, double testFees, double medicineFees)
+{
+    // add the service, test fees, and medicine together
+    return serviceCharge + testFees + medicineFees;
+}
