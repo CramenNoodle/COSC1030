717e1a911cb6a03cea95e8f8c1b663f1ffe677e0
Carmen Grajeda <carmengrajeda@westernwyoming.edu>
2025-04-25 20:52:46 +0000

    Ready to grade

--- string-length/main.cpp
+++ string-length/main.cpp
@@ -1,7 +1,53 @@
 //
-// Name
-// Date
+// Carmen Grajeda
+// 04/25/2025
 // String Length Programming Project
 // COSC 1030
 //
+
+// String Length Program 
+
+#include <iostream>
+
+using namespace std; 
+
+// Function prototype
+int countCharacters(char* str);
+
+int main()
+{
+    // Variables 
+    const int SIZE = 100; // Variable for max input length
+    char input[SIZE]; // Variable to hold the users input 
+
+    // ask the user for input 
+    cout << "Enter a string: ";
+    cin.getline(input, SIZE); 
+
+    // call function and store results
+    int numChars = countCharacters(input);
+
+    // Display the results
+    cout << "The number of characters in the string is: " << numChars << endl;
+
+    return 0;
+
+}
+
+/* This function takes a pointer to a char array 
+and counts how many characters are in the string, 
+excluding the null terminator.*/
+int countCharacters(char* str)
+{
+    int count = 0;
+
+    // loop through string 
+    while(str[count] != '\0')
+    {
+        count++; // count each character
+    }
+
+    // Return total number of characters
+    return count; 
+}
 
